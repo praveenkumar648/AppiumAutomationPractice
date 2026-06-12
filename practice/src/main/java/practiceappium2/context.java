9afb47ef62a5388d2f00bf556be9acb8c2b9e126
Praveen Ananthakumar <praveen.kumar@geekyants.com>
2026-06-12 09:57:49 +0530

    Completed the Context handing

--- practice/src/main/java/practiceappium2/context.java
+++ practice/src/main/java/practiceappium2/context.java
@@ -1,7 +1,9 @@
 package practiceappium2;
 
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Set;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.MobileBy;
@@ -11,10 +13,8 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class context {
 
-    public static void main(String[] args)
-            throws MalformedURLException, InterruptedException {
+    public static void main(String[] args) throws Exception {
 
-        // Desired Capabilities
         DesiredCapabilities dc = new DesiredCapabilities();
 
         dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
@@ -22,49 +22,71 @@ public class context {
         dc.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
         dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "17.0");
         dc.setCapability(MobileCapabilityType.NO_RESET, false);
+
         dc.setCapability("autoGrantPermissions", true);
+        dc.setCapability("appium:chromedriverAutodownload", true);
 
         dc.setCapability("appPackage", "io.appium.android.apis");
         dc.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
 
-        URL url = new URL("http://127.0.0.1:4723/");
-
         AndroidDriver<MobileElement> driver =
-                new AndroidDriver<>(url, dc);
+                new AndroidDriver<>(new URL("http://127.0.0.1:4723/"), dc);
 
         System.out.println("Application launched successfully");
 
+        // Click Views
+        driver.findElement(
+                MobileBy.AccessibilityId("Views"))
+                .click();
+
+        // Scroll to WebView and click
+        driver.findElement(
+                MobileBy.AndroidUIAutomator(
+                        "new UiScrollable(new UiSelector().scrollable(true))"
+                                + ".scrollIntoView(new UiSelector().description(\"WebView\"))"))
+                .click();
+
+        System.out.println("Opened WebView");
+
         Thread.sleep(3000);
 
-        // Click on Views
-        driver.findElement(
-                MobileBy.AccessibilityId("Views")
-        ).click();
+        // Get all contexts
+        Set<String> contexts = driver.getContextHandles();
 
-        System.out.println("Clicked on Views");
+        System.out.println("Available Contexts:");
+        for (String context : contexts) {
+            System.out.println(context);
+        }
 
-        //scroll down to WebView element and click on it
-        driver.findElement(
-    MobileBy.AndroidUIAutomator(
-        "new UiScrollable(new UiSelector().scrollable(true))" +
-        ".scrollIntoView(new UiSelector().description(\"WebView2\"))"
-    )
-    ).click();
+        // Switch to WebView
+        for (String context : contexts) {
+            if (context.contains("WEBVIEW")) {
+                driver.context(context);
+                System.out.println("Switched to : " + context);
+                break;
+            }
+        }
 
-    System.out.println("Scrolled and Clicked on WebView");
+        // Verify current context
+        System.out.println("Current Context : " + driver.getContext());
 
-        Thread.sleep(2000);
+        // ==========================
+        // WEBVIEW LOCATORS (Selenium)
+        // ==========================
 
-    driver.findElement(
-    MobileBy.AndroidUIAutomator(
-        "new UiSelector().text(\"i am a link\")")).click();
-    System.out.println("Clicked on the link");
+        driver.findElement(By.linkText("i am a link")).click();
 
-        String contexts = driver.getContext();
-        System.out.println(contexts);
+        // Alternative:
+        // driver.findElement(By.xpath("//a[text()='i am a link']")).click();
 
+        System.out.println("Clicked on WebView Link");
 
         Thread.sleep(3000);
+
+        // Switch back to Native App
+        driver.context("NATIVE_APP");
+
+        System.out.println("Switched back to Native App");
 
         driver.quit();
     }
